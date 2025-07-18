ceaf1874563866328396f82cbe7640a17a045919
dvillalba1 <dvillalba@excelsis.com.py>
2025-07-18 00:19:43 -0300

    no message

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
 set -o errexit
 
-# Fuerza la versión de Python especificada en runtime.txt
-PYTHON_VERSION=$(cat runtime.txt | cut -d '-' -f 2)
-echo "Usando Python $PYTHON_VERSION"
+# Forzar Python 3.11 aunque Render quiera usar otra versión
+PYTHON_VERSION="3.11"
+echo "===== ¡OBLIGANDO a usar Python $PYTHON_VERSION! ====="
 
-# Instalar dependencias con la versión correcta
+# Instalar dependencias
 python$PYTHON_VERSION -m pip install --upgrade pip setuptools wheel
 python$PYTHON_VERSION -m pip install -r requirements.txt
 
@@ -14,5 +13,3 @@ python$PYTHON_VERSION -m pip install -r requirements.txt
 python$PYTHON_VERSION manage.py makemigrations dashboard
 python$PYTHON_VERSION manage.py migrate --fake-initial
 python$PYTHON_VERSION manage.py collectstatic --noinput
-
-# Nota: Render ignora el runserver, usa el comando de inicio de Render
